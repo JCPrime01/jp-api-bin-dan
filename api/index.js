8f72a3a69024209f087de934217591c3580a58e1
JCPrime01 <vanzokiller@gmail.com>
2026-06-04 22:00:48 -0300

    chore: shareCode JRW0SJUJOLT -> IWK4KCRHAVT (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=JRW0SJUJOLT&afp=td' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=IWK4KCRHAVT&afp=td' });
     res.end();
   }
